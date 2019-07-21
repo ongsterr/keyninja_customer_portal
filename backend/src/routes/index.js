f24f1760c1d3b8843848aafe6bde2789091888b3
Chris Ong <ong.chris11@gmail.com>
2019-07-21 22:19:48 +1000

    chore: remove redundant middleware in backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,15 +3,4 @@ const customerRouter = require('./customers')
 
 router.use('/customers', customerRouter)
 
-router.use((err, req, res, next) => {
-  if (err.name == 'ValidationError') {
-    return res.status(422).json({
-      errors: Object.keys(err.errors).reduce((errors, key) => {
-        errors[key] = err.errors[key].message
-        return errors
-      }, {}),
-    })
-  }
-})
-
 module.exports = router
